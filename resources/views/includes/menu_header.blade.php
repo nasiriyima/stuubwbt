b1329ec789ec181d9a7d6e1f28d95cca833e8149
Sir Baslaw <baslaw46@gmail.com>
2016-06-10 14:52:55 +0100

    Changes on Nasir

--- resources/views/includes/menu_header.blade.php
+++ resources/views/includes/menu_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <!-- Logo -->
         <a class="logo" href="index.html">
-            <img src="{{ asset('public/assets/img/logo1-default.png')}}" width="220px" alt="Logo">
+            <img src="{{ asset('public/assets/img/logo1-default.jpg')}}" width="220px" alt="Logo">
         </a>
         <!-- End Logo -->
 
