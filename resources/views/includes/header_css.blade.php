3c20e5eef9c4536ebe3bacff853f4b2ea1636caf
nasiriyima <nasiriyima@yahoo.com>
2016-09-08 14:02:52 +0100

    Update header_css.blade.php

--- resources/views/includes/header_css.blade.php
+++ resources/views/includes/header_css.blade.php
@@ -1,22 +1,22 @@
         <!-- CSS Global Compulsory -->
-	<link rel="stylesheet" href="{{ asset('public/assets/plugins/bootstrap/css/bootstrap.min.css')}}">
-	<link rel="stylesheet" href="{{ asset('public/assets/css/style.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
 
 	<!-- CSS Header and Footer -->
-	<link rel="stylesheet" href="{{ asset('public/assets/css/headers/header-default.css')}}">
-	<link rel="stylesheet" href="{{ asset('public/assets/css/footers/footer-v1.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/css/headers/header-default.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/css/footers/footer-v1.css')}}">
 
 	<!-- CSS Implementing Plugins -->
-	<link rel="stylesheet" href="{{ asset('public/assets/plugins/animate.css')}}">
-	<link rel="stylesheet" href="{{ asset('public/assets/plugins/line-icons/line-icons.css')}}">
-	<link rel="stylesheet" href="{{ asset('public/assets/plugins/font-awesome/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/plugins/animate.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/plugins/line-icons/line-icons.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/plugins/font-awesome/css/font-awesome.min.css')}}">
 
 	<!-- CSS Theme -->
-	<link rel="stylesheet" href="{{ asset('public/assets/css/theme-colors/default.css')}}" id="style_color">
-	<link rel="stylesheet" href="{{ asset('public/assets/css/theme-skins/dark.css')}}">
-	<link rel="stylesheet" href="{{ asset('public/assets/plugins/login-signup-modal-window/css/style.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/css/theme-colors/default.css')}}" id="style_color">
+	<link rel="stylesheet" href="{{ asset('assets/css/theme-skins/dark.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/plugins/login-signup-modal-window/css/style.css')}}">
 
 	<!-- CSS Customization -->
-	<link rel="stylesheet" href="{{ asset('public/assets/css/custom.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/css/custom.css')}}">
 	@yield('pagecss')
         
